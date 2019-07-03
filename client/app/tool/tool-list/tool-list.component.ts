955d033e27c640e36303c270c88276d117289395
Rob Brennan <rob@therobbrennan.com>
2019-07-02 17:14:42 -0700

    Wrapped create tool with required user permission

--- client/app/tool/tool-list/tool-list.component.ts
+++ client/app/tool/tool-list/tool-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, AfterViewInit, ViewChild, ViewChildren, ContentChildren, QueryList, forwardRef } from '@angular/core';
+import { Component, OnInit, OnDestroy, AfterViewInit, ViewChild, ViewChildren, ContentChildren, QueryList, forwardRef } from '@angular/core';
 import { Observable, combineLatest } from 'rxjs';
 import { map, switchMap, tap } from 'rxjs/operators';
 import { Router } from '@angular/router';
@@ -10,6 +10,14 @@ import { orderBy } from 'lodash/fp';
 import { NotificationService } from '../../../components/notification/notification.service'
 // import { ToolNewComponent } from '../tool-new/tool-new.component';
 
+// User authorization and permissions
+import { AuthService } from '../../../components/auth/auth.service';
+import { UserService } from '../../../components/auth/user.service';
+import { UserPermissionDataService, UserPermissions } from '../../../components/auth/user-permission-data.service';
+import { User } from '../../../../shared/interfaces/user.model';
+import { UserPermission } from '../../../../shared/interfaces/user-permission.model';
+import { Subscription } from 'rxjs';
+
 @Component({
     selector: 'tool-list',
     template: require('./tool-list.html'),
@@ -17,23 +25,38 @@ import { NotificationService } from '../../../components/notification/notificati
 })
 export class ToolListComponent implements OnInit, AfterViewInit {
     private tools: Observable<Tool[]>;
-
     // @ViewChild(ToolNewComponent, { static: false }) newTool: ToolNewComponent;
     private createNewTool = false
 
-    static parameters = [Router, FormBuilder, PageTitleService, ToolService, NotificationService];
+    currentUser: User;
+    authInfoSub: Subscription;
+    private permissions: Observable<UserPermissions>;
+
+    static parameters = [Router, FormBuilder, PageTitleService, ToolService, NotificationService, AuthService, UserService, UserPermissionDataService];
     constructor(private router: Router, private formBuilder: FormBuilder,
         private pageTitleService: PageTitleService,
         private toolService: ToolService,
-        private notificationService: NotificationService) {
+        private notificationService: NotificationService,
+        private authService: AuthService,
+        private userService: UserService,
+        private userPermissionDataService: UserPermissionDataService) {
         this.tools = this.toolService.getTools()
             .pipe(
                 map(tools => orderBy('name', 'asc', tools))
             );
+        this.authInfoSub = this.authService.authInfo()
+            .subscribe(authInfo => {
+                this.currentUser = authInfo.user;
+            });
+        this.permissions = this.userPermissionDataService.getPermissions();
     }
 
     ngOnInit() {
         this.pageTitleService.title = 'Tools';
+    }
+
+    ngOnDestroy() {
+        this.authInfoSub.unsubscribe();
     }
 
     ngAfterViewInit() {
