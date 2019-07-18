608a37bfb64786ade764fcb3152c6dd892d91075
Rob Brennan <rob@therobbrennan.com>
2019-07-18 14:49:40 -0700

    Reorganized entity permission code

--- server/api/entity-permission/index.js
+++ server/api/entity-permission/index.js
@@ -1,22 +1,26 @@
 var express = require('express');
 var controller = require('./entity-permission.controller');
 import * as auth from '../../auth/auth.service';
+import { accessTypes } from '../../config/environment/shared';
 
 var router = express.Router();
+const ADMIN_ROLE_FOR_ENTITY = accessTypes.ADMIN.value;
 
+// Entity permissions
 router.get('/', auth.hasRole('admin'), controller.index);
-// router.get('/:id', controller.show);
-router.post('/', auth.isAuthenticated(), controller.create);
-// router.put('/:id', controller.upsert);
-router.patch('/:id', auth.isAuthenticated(), controller.patch);
-router.delete('/:id', auth.isAuthenticated(), controller.destroy);
-
+router.post('/', auth.isAuthenticated(), controller.create); // WIP #231 - Use auth.hasPermissionForEntity(ADMIN_ROLE_FOR_ENTITY)
 router.get('/mine', auth.isAuthenticated(), controller.indexMine);
-router.get('/entity/:id', auth.isAuthenticated(), controller.indexEntityPermissions);  // TODO: Must be admin of entity
-// router.put('/:id/access', auth.isAuthenticated(), controller.changeAccess);
-// router.post('/entity/:id', auth.isAuthenticated(), controller.createEntityPermission);   // TODO: Must be admin of entity
-// router.delete('/entity/:id', auth.isAuthenticated(), controller.destroyEntityPermission);  // TODO: Must be admin of entity
 
-// auth.hasEntityPermission('EDIT', projectId)
+// Specific entity permission object - defining access for a specific user on a known entity
+router.patch('/:id', auth.isAuthenticated(), controller.patch); // WIP #231 - Use auth.hasPermissionForEntity(ADMIN_ROLE_FOR_ENTITY)
+router.delete('/:id', auth.isAuthenticated(), controller.destroy); // WIP #231 - Use auth.hasPermissionForEntity(ADMIN_ROLE_FOR_ENTITY) with a twist - a user that matches req.body.user._id should be able to remove themselves from an entity (project)
+// router.get('/:id', controller.show);
+// router.put('/:id', controller.upsert);
+// router.put('/:id/access', auth.isAuthenticated(), controller.changeAccess);
+
+// A specific entity (project, etc)
+router.get('/entity/:id', auth.hasPermissionForEntity(ADMIN_ROLE_FOR_ENTITY), controller.indexEntityPermissions); // WIP #231 - Use auth.hasPermissionForEntity(ADMIN_ROLE_FOR_ENTITY)
+// router.post('/entity/:id', auth.hasPermissionForEntity(ADMIN_ROLE_FOR_ENTITY), controller.createEntityPermission); // WIP #231 - Use auth.hasPermissionForEntity(ADMIN_ROLE_FOR_ENTITY)
+// router.delete('/entity/:id', auth.hasPermissionForEntity(ADMIN_ROLE_FOR_ENTITY), controller.destroyEntityPermission); // WIP #231 - Use auth.hasPermissionForEntity(ADMIN_ROLE_FOR_ENTITY)
 
 module.exports = router;
