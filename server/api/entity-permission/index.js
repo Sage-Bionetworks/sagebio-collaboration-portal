539622d9ade2ad1c24a49146c0e38644cf9796e5
Rob Brennan <rob@therobbrennan.com>
2019-07-19 12:10:22 -0700

    Renamed constant containing the admin access type

--- server/api/entity-permission/index.js
+++ server/api/entity-permission/index.js
@@ -4,23 +4,23 @@ import * as auth from '../../auth/auth.service';
 import { accessTypes } from '../../config/environment/shared';
 
 var router = express.Router();
-const ADMIN_ROLE_FOR_ENTITY = accessTypes.ADMIN.value;
+const ADMIN_ACCESS = accessTypes.ADMIN.value;
 
 // Entity permissions
-router.get('/', auth.hasPermissionForEntity(ADMIN_ROLE_FOR_ENTITY), controller.index);
-router.post('/', auth.hasPermissionForEntity(ADMIN_ROLE_FOR_ENTITY), controller.create);
+router.get('/', auth.hasPermissionForEntity(ADMIN_ACCESS), controller.index);
+router.post('/', auth.hasPermissionForEntity(ADMIN_ACCESS), controller.create);
 router.get('/mine', auth.isAuthenticated(), controller.indexMine);
 
 // Specific entity permission object - defining access for a specific user on a known entity
-router.patch('/:id', auth.hasPermissionForEntity(ADMIN_ROLE_FOR_ENTITY), controller.patch);
-router.delete('/:id', auth.hasPermissionForEntity(ADMIN_ROLE_FOR_ENTITY), controller.destroy);
+router.patch('/:id', auth.hasPermissionForEntity(ADMIN_ACCESS), controller.patch);
+router.delete('/:id', auth.hasPermissionForEntity(ADMIN_ACCESS), controller.destroy);
 // router.get('/:id', controller.show);
 // router.put('/:id', controller.upsert);
 // router.put('/:id/access', auth.isAuthenticated(), controller.changeAccess);
 
 // A specific entity (project, etc)
-router.get('/entity/:id', auth.hasPermissionForEntity(ADMIN_ROLE_FOR_ENTITY), controller.indexEntityPermissions);
-// router.post('/entity/:id', auth.hasPermissionForEntity(ADMIN_ROLE_FOR_ENTITY), controller.createEntityPermission);
-// router.delete('/entity/:id', auth.hasPermissionForEntity(ADMIN_ROLE_FOR_ENTITY), controller.destroyEntityPermission);
+router.get('/entity/:id', auth.hasPermissionForEntity(ADMIN_ACCESS), controller.indexEntityPermissions);
+// router.post('/entity/:id', auth.hasPermissionForEntity(ADMIN_ACCESS), controller.createEntityPermission);
+// router.delete('/entity/:id', auth.hasPermissionForEntity(ADMIN_ACCESS), controller.destroyEntityPermission);
 
 module.exports = router;
