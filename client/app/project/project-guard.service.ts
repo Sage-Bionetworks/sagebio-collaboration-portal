92a5bc44f017e264be444a58ceab5b77affd2ea2
tschaffter <thomas.schaffter@gmail.com>
2019-07-23 21:53:54 +0000

    Add project guard (close #250)

---
+++ client/app/project/project-guard.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router, ActivatedRouteSnapshot } from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { map, catchError, tap } from 'rxjs/operators';
+import { UserPermissionDataService } from 'components/auth/user-permission-data.service';
+import { NotificationService } from 'components/notification/notification.service';
+
+@Injectable()
+export class ProjectGuard implements CanActivate {
+    static parameters = [Router, UserPermissionDataService, NotificationService];
+    constructor(private router: Router,
+        private userPermissionDataService: UserPermissionDataService,
+        private notificationService: NotificationService) { }
+
+    canActivate(route: ActivatedRouteSnapshot):
+        Observable<boolean> | boolean {
+
+        const entityId = route.params.id;
+        return this.userPermissionDataService.permissions()
+            .pipe(
+                map(permissions => permissions.canReadEntity(entityId, 'project')),
+                tap(canAccess => {
+                    if (!canAccess) {
+                        this.notificationService.info('You do not have access to this project.');
+                        this.router.navigate(['']);
+                    }
+                }),
+                catchError(err => {
+                    console.log(err);
+                    return of(false);
+                })
+            );
+    }
+}
--- client/components/auth/auth.module.ts
+++ client/components/auth/auth.module.ts
@@ -3,7 +3,6 @@ import { AuthService } from './auth.service';
 import { UserService } from './user.service';
 import { TokenService } from './token.service';
 import { AuthGuard } from './auth-guard.service';
-import { EntityAuthGuard } from './entity-auth-guard.service';
 import { UserPermissionService } from './user-permission.service';
 import { EntityPermissionService } from './entity-permission.service';
 import { UserPermissionDataService } from './user-permission-data.service';
@@ -14,7 +13,6 @@ import { UserPermissionDataService } from './user-permission-data.service';
         UserService,
         TokenService,
         AuthGuard,
-        EntityAuthGuard,
         UserPermissionDataService,
         UserPermissionService,
         EntityPermissionService
