6e04b87c8cfab1f91a121ed8cd0f634165fc6fdd
James Eddy <james.a.eddy@gmail.com>
2019-07-25 16:49:21 -0700

    add direction selector for activity sidenav

--- client/components/activity/activity-sidenav/activity-sidenav.component.ts
+++ client/components/activity/activity-sidenav/activity-sidenav.component.ts
@@ -1,31 +1,63 @@
-import { Component, OnDestroy } from '@angular/core';
-import { BehaviorSubject, Subscription } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { orderBy } from 'lodash/fp';
+import { Component, OnDestroy, AfterViewInit, ViewChildren, QueryList } from '@angular/core';
+import { BehaviorSubject, Subscription, combineLatest } from 'rxjs';
+import { map, take, switchMap } from 'rxjs/operators';
+import { flow, keyBy, mapValues, values, find, orderBy } from 'lodash/fp';
 import { SecondarySidenavService } from 'components/sidenav/secondary-sidenav/secondary-sidenav.service';
 import { SocketService } from 'components/socket/socket.service';
 import { ProvenanceService } from 'components/provenance/provenance.service';
 import { Insight } from 'models/insights/insight.model';
+import config from '../../../../client/app/app.constants';
+import { Filter } from 'components/filters/filter.model';
+import { FiltersComponent } from 'components/filters/filters.component';
 
 @Component({
     selector: 'activity-sidenav',
     template: require('./activity-sidenav.html'),
     styles: [require('./activity-sidenav.scss')]
 })
-export class ActivitySidenavComponent implements OnDestroy {
+export class ActivitySidenavComponent implements OnDestroy, AfterViewInit {
+    @ViewChildren(FiltersComponent) filters: QueryList<FiltersComponent>;
     private insight: Insight;
     // private activity: BehaviorSubject<Insight> = new BehaviorSubject<Insight>();
     private provenanceGraph: any;
+    // // private states: State[];
+    // // private reports: Insight[];
+    // private insights: Insight[] = [];
+
+    private activityDirectionFilters: Filter[] = [];
+
 
     static parameters = [SecondarySidenavService, ProvenanceService, SocketService];
     constructor(private sidenavService: SecondarySidenavService,
         private provenanceService: ProvenanceService,
         private socketService: SocketService) {
+            this.activityDirectionFilters = values(config.activityDirectionFilters);
+
             // this.activity = this.provenanceService.getProvenanceGraph('created_at', 'desc', 1)
             //     .subscribe(graph => {
             //         this.provenanceGraph = graph;
             //     });
         }
+
+    ngAfterViewInit() {
+        let selectedFilters = this.filters.map(f => f.getSelectedFilter());
+        combineLatest(...selectedFilters)
+            .pipe(
+                map(myFilters =>
+                    flow([
+                        keyBy('group'),
+                        mapValues('value')
+                    ])(myFilters)
+                )
+            )
+            .subscribe(direction => {
+                this.provenanceService.getProvenanceGraphByReference(this.insight._id, direction.activityDirection, 'created_at', 'desc', 1)
+                    .subscribe(activity => {
+                        // this.socketService.syncItemSubject(`activity:${insight._id}:insight`, this.activity);
+                        this.provenanceGraph = activity;
+                    });
+            })
+    }
 
     ngOnDestroy() {
         this.socketService.unsyncUpdates(`activity:${this.insight._id}:insight`);
@@ -33,11 +65,11 @@ export class ActivitySidenavComponent implements OnDestroy {
 
     setInsight(insight: Insight): void {
         if (insight) {
-            this.provenanceService.getProvenanceGraphByReference(insight._id, 'up', 'created_at', 'desc', 1)
+            this.provenanceService.getProvenanceGraphByReference(insight._id, 'down', 'created_at', 'desc', 1)
                 .subscribe(activity => {
                     // this.socketService.syncItemSubject(`activity:${insight._id}:insight`, this.activity);
                     this.provenanceGraph = activity;
-                    this.insight = insight
+                    this.insight = insight;
                 });
         }
     }
