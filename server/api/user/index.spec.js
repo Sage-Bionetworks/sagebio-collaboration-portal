b8ccd9c858070794ed429a7d8c917489a81d75b7
Rob Brennan <rob@therobbrennan.com>
2019-07-24 22:31:06 -0700

    Added tests to verify PATCH api for user data

--- server/api/user/index.spec.js
+++ server/api/user/index.spec.js
@@ -8,7 +8,8 @@ var userCtrlStub = {
     me: 'userCtrl.me',
     changePassword: 'userCtrl.changePassword',
     show: 'userCtrl.show',
-    create: 'userCtrl.create'
+    create: 'userCtrl.create',
+    patch: 'userCtrl.patch'
 };
 
 var authServiceStub = {
@@ -24,7 +25,8 @@ var routerStub = {
     get: sinon.spy(),
     put: sinon.spy(),
     post: sinon.spy(),
-    delete: sinon.spy()
+    delete: sinon.spy(),
+    patch: sinon.spy(),
 };
 
 // require the index with our stubbed out modules
@@ -71,6 +73,14 @@ describe('User API Router:', function() {
         it('should be authenticated and route to user.controller.changePassword', function() {
             expect(routerStub.put
                 .withArgs('/:id/password', 'authService.isAuthenticated', 'userCtrl.changePassword')
+                ).to.have.been.calledOnce;
+        });
+    });
+
+    describe('PATCH /api/users/:id', function() {
+        it('should be authenticated and route to user.controller.patch', function() {
+            expect(routerStub.patch
+                .withArgs('/:id', 'authService.isAuthenticated', 'userCtrl.patch')
                 ).to.have.been.calledOnce;
         });
     });
