17650dc23219b09ab968cc35800db7adc05b78ff
Rob Brennan <rob@therobbrennan.com>
2019-07-30 13:07:50 -0700

    Created Thread interface

--- shared/interfaces/messaging/thread.model.ts
+++ shared/interfaces/messaging/thread.model.ts
@@ -1,1 +1,11 @@
-// WIP #49 - Create the shared client Thread interface
+import { UserProfile } from '../auth/user-profile.model';
+
+export interface Thread {
+    _id?: string;
+    title: string;
+    entityId: string;
+    entityType: string;
+    createdAt: string;
+    updatedAt: string;
+    createdBy: UserProfile;
+}
