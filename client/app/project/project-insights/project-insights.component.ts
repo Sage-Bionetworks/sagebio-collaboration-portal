208ed7f2fad31e56c2bb66d5a4b5f79779edcd69
tschaffter <thomas.schaffter@gmail.com>
2019-07-31 20:39:33 +0000

    Fix insight listing issue

--- client/app/project/project-insights/project-insights.component.ts
+++ client/app/project/project-insights/project-insights.component.ts
@@ -1,23 +1,22 @@
-import { Component, AfterViewInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { InsightService } from '../../insight/insight.service';
 import { Insight } from 'models/entities/insights/insight.model';
 
 @Component({
     selector: 'project-insights',
     template: require('./project-insights.html'),
-    styles: [require('./project-insights.scss')],
-    changeDetection: ChangeDetectionStrategy.OnPush,
+    styles: [require('./project-insights.scss')]
 })
-export class ProjectInsightsComponent implements AfterViewInit {
-    insights: Insight[]
+export class ProjectInsightsComponent implements OnInit {
+    private insights: Insight[];
     static parameters = [InsightService];
     constructor(private insightService: InsightService) { }
 
-    ngAfterViewInit() {
+    ngOnInit() {
         this.insightService.getInsights()
             .subscribe(insights => {
                 console.log('insights: ', insights);
-                this.insights = insights
-            })
+                this.insights = insights;
+            });
     }
 }
