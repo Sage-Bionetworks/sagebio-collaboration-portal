42b1a34d50f35d51a4d66bdf6d113f1a3e0f3342
Rob Brennan <rob@therobbrennan.com>
2019-08-07 13:14:43 -0700

    Updated model to reflect optional entity properties

--- shared/interfaces/messaging/thread.model.ts
+++ shared/interfaces/messaging/thread.model.ts
@@ -1,10 +1,10 @@
 import { UserProfile } from '../auth/user-profile.model';
 
 export interface Thread {
-    _id?: string;
+    _id: string;
     title: string;
-    entityId: string;
-    entityType: string;
+    entityId?: string;
+    entityType?: string;
     createdAt: string;
     updatedAt: string;
     createdBy: UserProfile;
