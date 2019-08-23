9a02092a9e9e463717b116ea577cb575eb6be3e6
James Eddy <james.a.eddy@gmail.com>
2019-08-22 22:06:21 -0700

    add necessary fields to create basic activity types

--- client/components/activity/activity-new/activity-new.component.ts
+++ client/components/activity/activity-new/activity-new.component.ts
@@ -1,13 +1,12 @@
 import { Component, Output, EventEmitter } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
 import { ProvenanceService } from 'components/provenance/provenance.service';
 import { Activity } from 'models/provenance/activity.model';
 import { PageTitleService } from 'components/page-title/page-title.service';
 import config from '../../../app/app.constants';
 import { ProjectDataService } from '../../../app/project/project-data.service';
 import { Project } from 'models/entities/project.model';
-
 
 @Component({
     selector: 'activity-new',
@@ -45,6 +44,12 @@ export class ActivityNewComponent {
                 Validators.minLength(config.models.activity.title.minlength),
                 Validators.maxLength(config.models.activity.title.maxlength)
             ]],
+            used: ['', [
+                Validators.required
+            ]],
+            generated: ['', [
+                Validators.required
+            ]],
             description: ['', [
                 Validators.required,
                 Validators.minLength(config.models.activity.description.minlength),
@@ -59,18 +64,41 @@ export class ActivityNewComponent {
     }
 
     createNewActivity(): void {
-        let newActivity = this.newForm.value;
-        newActivity.description = JSON.stringify(newActivity.description);
-        // newActivity.projectId = this.project._id;
+        let currentUser = {
+            _id: '5cb7acea2d718614d81bb97f',
+            name: 'Admin'
+        }
+        let currentUsed = [{
+            name: this.newForm.value.used,
+            targetId: this.newForm.value.used,
+            targetVersionId: '1',
+            class: 'Resource',
+            subclass: 'State'
+        }]
+        let currentGenerated = [{
+            name: this.newForm.value.generated,
+            targetId: this.newForm.value.generated,
+            targetVersionId: '1',
+            class: 'Insight',
+            subclass: 'Report'
+        }]
+        let currentAgents = [{
+            userId: currentUser._id,
+            name: currentUser.name,
+        }]
+        let currentDescription = JSON.stringify(this.newForm.value.description);
+        let newActivity = {
+            name: this.newForm.value.title,
+            class: this.newForm.value.activityType,
+            used: currentUsed,
+            generated: currentGenerated,
+            agents: currentAgents,
+            description: currentDescription
+        }
+        console.log('ACTIVITY', newActivity);
         this.provenanceService.createProvenanceActivity(newActivity)
             .subscribe(activity => {
                 this.newActivity.emit(activity);
-                // this.captureProvActivity.save({
-                //     generatedName: activity.title,
-                //     generatedTargetId: activity._id,
-                //     generatedClass: 'Activity',
-                //     generatedSubClass: activity.activityType
-                // });
             }, err => {
                 console.log(err);
                 this.errors.newActivity = err.message;
