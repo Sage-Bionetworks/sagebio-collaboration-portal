1a4923e0e2bc6a56f67415f84c713aba4fcc5353
tschaffter <thomas.schaffter@gmail.com>
2019-08-25 16:30:03 -0700

    Add measurable directive to measure the width of a container

---
+++ client/components/directives/measurable.directive.ts
@@ -0,0 +1,32 @@
+import { Directive, ElementRef } from '@angular/core';
+import { Observable, fromEvent, merge, of } from 'rxjs';
+import {
+    debounceTime,
+    distinctUntilChanged,
+    map
+} from 'rxjs/operators';
+
+@Directive({
+    selector: '[appMeasurable]'
+})
+export class MeasurableDirective {
+    private resize$: Observable<Window>;
+
+    constructor(private elem: ElementRef) {}
+
+    ngOnInit() {
+        this.resize$ = fromEvent<Window>(window, 'resize');
+    }
+
+    width(): Observable<number> {
+        return merge(this.resize$, of(1))  // of added to fire at subscribe time
+            .pipe(
+                map(() => {
+                    const bounds = <ClientRect>this.elem.nativeElement.getBoundingClientRect();
+                    return bounds.width;
+                }),
+                debounceTime(100),
+                distinctUntilChanged()
+            );
+    }
+}
