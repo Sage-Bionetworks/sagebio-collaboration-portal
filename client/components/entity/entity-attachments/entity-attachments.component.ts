611ba6e5ebaea5f9a6256992c017ede1bf5e6dbd
Rob Brennan <rob@therobbrennan.com>
2019-08-30 11:07:21 -0700

    Created a placeholder entity-attachments component

---
+++ client/components/entity/entity-attachments/entity-attachments.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'entity-attachments',
+    template: require('./entity-attachments.html'),
+    styles: [require('./entity-attachments.scss')],
+})
+export class EntityAttachmentsComponent {
+    static parameters = [];
+    constructor () {}
+
+}
