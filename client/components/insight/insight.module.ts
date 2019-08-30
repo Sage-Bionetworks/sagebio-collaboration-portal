1a182c93d05fc02839b3afa6a6d80db4e5ec0970
Rob Brennan <rob@therobbrennan.com>
2019-08-30 11:08:00 -0700

    Importing EntityModule into Insights module

--- client/components/insight/insight.module.ts
+++ client/components/insight/insight.module.ts
@@ -5,6 +5,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppQuillModule } from 'components/quill/app-quill.module';
 import { MaterialModule } from 'components/material/material.module';
 import { ActivityModule } from 'components/activity/activity.module';
+import { EntityModule } from 'components/entity/entity.module';
 
 import { InsightPageComponent } from './insight-page/insight-page.component';
 import { InsightNewComponent } from './insight-new/insight-new.component';
@@ -20,6 +21,7 @@ import { RouterModule } from '@angular/router';
         MaterialModule,
         AppQuillModule,
         ActivityModule,
+        EntityModule,
         RouterModule,
     ],
     declarations: [
