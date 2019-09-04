552c7e539f7dc3c0587b56a90984522f0f1f0458
Rob Brennan <rob@therobbrennan.com>
2019-09-04 11:15:27 -0700

    Commenting attachments declaration in Memo and Report models

--- server/api/insight/models/memo.model.js
+++ server/api/insight/models/memo.model.js
@@ -2,10 +2,10 @@ import mongoose from 'mongoose';
 import Insight from './insight.model';
 
 var MemoSchema = new mongoose.Schema({
-    attachments: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Insight'
-    }]
+    // attachments: [{
+    //     type: mongoose.Schema.Types.ObjectId,
+    //     ref: 'Insight'
+    // }]
 });
 
 export default Insight.discriminator('memo', MemoSchema);
