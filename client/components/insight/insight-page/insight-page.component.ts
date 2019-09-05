44d591731d0e939975ce582af49fad8d5c920bcf
Rob Brennan <rob@therobbrennan.com>
2019-09-05 14:33:40 -0700

    Insight page now displays entity attachments in edit mode if the user is an admin

--- client/components/insight/insight-page/insight-page.component.ts
+++ client/components/insight/insight-page/insight-page.component.ts
@@ -4,11 +4,14 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 import { InsightService } from 'components/insight/insight.service';
 // import { StateService } from '../../state/state.service';
+import { UserPermissionDataService, UserPermissions } from 'components/auth/user-permission-data.service';
 
+import { EntityAttachmentMode } from 'models/entities/entity.model';
 import { Insight } from 'models/entities/insights/insight.model';
 
 import { PageTitleService } from 'components/page-title/page-title.service';
 import { NotificationService } from 'components/notification/notification.service';
+import { Observable, Subscription } from 'rxjs';
 import { switchMap, debounceTime, distinctUntilChanged } from 'rxjs/operators';
 import config from '../../../app/app.constants';
 import { ObjectValidators } from 'components/validation/object-validators';
@@ -20,41 +23,71 @@ import { ObjectValidators } from 'components/validation/object-validators';
 })
 export class InsightPageComponent implements OnInit, OnDestroy {
     @Output() insightOutput = new EventEmitter<Insight>();
+    private isAdmin = false;
+    private mode = EntityAttachmentMode.DISPLAY;
+    private userPermissionsSub: Subscription;
+    private permissions: Observable<UserPermissions>;
     private insight: Insight;
     private form: FormGroup;
     private errors = {
-        updateDescription: undefined
+        updateDescription: undefined,
     };
     private entityType = config.entityTypes.INSIGHT.value
 
-    static parameters = [Router, ActivatedRoute, FormBuilder, PageTitleService, InsightService, NotificationService];
+    static parameters = [
+        Router,
+        ActivatedRoute,
+        FormBuilder,
+        PageTitleService,
+        InsightService,
+        NotificationService,
+        UserPermissionDataService,
+    ];
+
     constructor(
         private router: Router,
         private route: ActivatedRoute,
         private formBuilder: FormBuilder,
         private pageTitleService: PageTitleService,
         private insightService: InsightService,
-        private notificationService: NotificationService) {
-
+        private notificationService: NotificationService,
+        private userPermissionDataService: UserPermissionDataService
+    ) {
         this.form = formBuilder.group({
-            description: ['', [
-                // Validators.required,
-                // ObjectValidators.jsonStringifyMinLength(config.models.insight.description.minlength),
-                ObjectValidators.jsonStringifyMaxLength(config.models.insight.description.maxlength)
-            ]]
+            description: [
+                '',
+                [
+                    // Validators.required,
+                    // ObjectValidators.jsonStringifyMinLength(config.models.insight.description.minlength),
+                    ObjectValidators.jsonStringifyMaxLength(config.models.insight.description.maxlength),
+                ],
+            ],
         });
 
+        this.userPermissionsSub = this.userPermissionDataService.permissions().subscribe(
+            userPermissions => {
+                this.isAdmin = userPermissions.isAdmin();
+
+                // TODO: Apply permissions so that users other than Admin can edit
+                if (this.isAdmin) {
+                    this.mode = EntityAttachmentMode.EDIT;
+                }
+            },
+            err => console.log(err)
+        );
+
         this.route.params
-            .pipe(
-                switchMap(params => this.insightService.getInsight(params.insightId))
-            )
+            .pipe(switchMap(params => this.insightService.getInsight(params.insightId)))
             .subscribe(insight => {
-                if (insight.description) {  // TODO: should be required
+                if (insight.description) {
+                    // TODO: should be required
                     try {
                         this.form.get('description').setValue(JSON.parse(insight.description));
                     } catch (e) {
                         // the description is likely a string if specified from a tool
-                        this.form.get('description').setValue(JSON.parse(`{\"ops\":[{\"insert\":\"${insight.description}\"}]}`));
+                        this.form
+                            .get('description')
+                            .setValue(JSON.parse(`{\"ops\":[{\"insert\":\"${insight.description}\"}]}`));
                     }
                 }
                 this.insight = insight;
@@ -63,33 +96,33 @@ export class InsightPageComponent implements OnInit, OnDestroy {
     }
 
     ngOnInit() {
-        this.form
-            .controls
-            .description
-            .valueChanges.pipe(
+        this.form.controls.description.valueChanges
+            .pipe(
                 debounceTime(50),
                 distinctUntilChanged()
             )
-            .subscribe((data) => {
+            .subscribe(data => {
                 this.errors.updateDescription = undefined;
             });
     }
 
-    ngOnDestroy() { }
+    ngOnDestroy() {}
 
     updateDescription(): void {
         let description = JSON.stringify(this.form.get('description').value);
         // console.log('description', description);
         // console.log('DESCRIPTION', description);
         try {
-            this.insightService.updateInsightDescription(this.insight, description)
-                .subscribe(insight => {
+            this.insightService.updateInsightDescription(this.insight, description).subscribe(
+                insight => {
                     this.notificationService.info('The description has been successfully saved');
-                }, err => {
+                },
+                err => {
                     console.log(err);
                     // this.errors.updateDescription = err.message;
-                });
-        } catch (e) { }
+                }
+            );
+        } catch (e) {}
         // try {
         //     this.insightService.updateStateDescription(this.insight, description)
         //         .subscribe(insight => {
