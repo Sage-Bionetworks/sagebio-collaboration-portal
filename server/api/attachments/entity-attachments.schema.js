35b988753783485026b44ae4b1dca6383f8bc6f6
Rob Brennan <rob@therobbrennan.com>
2019-09-05 18:25:03 -0700

    Cleaned up EntityAttachmentsSchema

--- server/api/attachments/entity-attachments.schema.js
+++ server/api/attachments/entity-attachments.schema.js
@@ -3,20 +3,6 @@ import InsightAttachmentSchema from './insight-attachment.schema';
 import ResourceAttachmentSchema from './resource-attachment.schema';
 
 var EntityAttachmentsSchema = new mongoose.Schema({
-    // TODO Change to subEntityType (join both Insights and Resources types e.g. Report, State, Dashboard)
-    model: {
-        type: String,
-        required: true
-    },
-    // TODO Change source to entityType
-    source: {
-        type: String,
-        required: true,
-    },
-    // WIP Remove name and use the entityId when lookup is implemented
-    name: {
-        type: String,
-    },
     insights: [InsightAttachmentSchema],
     resources: [ResourceAttachmentSchema],
 });
