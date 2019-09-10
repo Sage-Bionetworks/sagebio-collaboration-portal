de16174249d2e981988883d21d034827482c8b9d
tschaffter <thomas.schaffter@gmail.com>
2019-09-09 19:58:04 -0700

    Add entity-list to Project > Insights; need to filter out the insights that are not part of the project

--- client/app/project/project-insights/project-insights.component.ts
+++ client/app/project/project-insights/project-insights.component.ts
@@ -7,11 +7,13 @@ import { ProjectDataService } from '../project-data.service';
 import { Project } from 'models/entities/project.model';
 import config from '../../../app/app.constants';
 import { NotificationService } from 'components/notification/notification.service';
+import { Router } from '@angular/router';
+import { PageTitleService } from 'components/page-title/page-title.service';
 
 @Component({
     selector: 'project-insights',
     template: require('./project-insights.html'),
-    styles: [require('./project-insights.scss')]
+    styles: [require('./project-insights.scss')],
 })
 export class ProjectInsightsComponent implements OnInit {
     private project: Project;
@@ -19,29 +21,44 @@ export class ProjectInsightsComponent implements OnInit {
     private insightTypeFilters = config.insightTypeFilters;
     private showNewInsightForm = false;
 
-    static parameters = [InsightService, ProjectDataService, NotificationService];
-    constructor(private insightService: InsightService,
+    static parameters = [Router, PageTitleService, InsightService, ProjectDataService, NotificationService];
+    constructor(
+        private router: Router,
+        private pageTitleService: PageTitleService,
+        private insightService: InsightService,
         private projectDataService: ProjectDataService,
-        private notificationService: NotificationService) { }
+        private notificationService: NotificationService
+    ) {
+        this.insightTypeFilters = config.insightTypeFilters;
+    }
 
     ngOnInit() {
         this.projectDataService.project()
             .subscribe(project => {
-                this.project = project;
-                const selectedFilter = config.insightTypeFilters.find(filter => filter.active);
-                const defaultQuery = { insightType: selectedFilter.value };
-                this.onFilterChange(defaultQuery);
+                if (project) {
+                    this.pageTitleService.title = `${project.title} - Insights`;
+                }
+                // this.project = project;
+                // const selectedFilter = config.insightTypeFilters.find(filter => filter.active);
+                // const defaultQuery = { insightType: selectedFilter.value };
+                // this.onFilterChange(defaultQuery);
             }, err => console.error(err));
     }
 
-    onFilterChange(query) {
-        if (this.project) {
-            this.insightService.queryByProject(this.project, query)
-                .subscribe(insights => {
-                    this.insights = orderBy(insights, 'createdAt', 'asc');
-                }, err => {
-                    console.log(err);
-                });
+    // onFilterChange(query) {
+    //     if (this.project) {
+    //         this.insightService.queryByProject(this.project, query)
+    //             .subscribe(insights => {
+    //                 this.insights = orderBy(insights, 'createdAt', 'asc');
+    //             }, err => {
+    //                 console.log(err);
+    //             });
+    //     }
+    // }
+
+    onEntityClick(insight: Insight) {
+        if (insight) {
+            this.router.navigate(['/projects', insight.projectId, 'insights', insight._id]);
         }
     }
 
