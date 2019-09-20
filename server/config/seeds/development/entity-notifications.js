a87c217dbd5b4a9f95a7295b976b063a47fcffed
tschaffter <thomas.schaffter@gmail.com>
2019-09-19 17:06:42 -0700

    Fix user notification seeds

--- server/config/seeds/development/entity-notifications.js
+++ server/config/seeds/development/entity-notifications.js
@@ -1,23 +1,24 @@
 import { report2Id, report3Id } from './reports';
 import { adminUserId, testUserId } from './users';
-
 import { notificationTypes, entityTypes } from '../../environment/shared';
 
-let entityNotifications = [{
-    notificationType: notificationTypes.ENTITY_NOTIFICATION.value,
-    user: testUserId,
-    createdBy: adminUserId,
-    archived: false,
-    entityId: report2Id,
-    entityType: entityTypes.INSIGHT.value
-},
-{
-    notificationType: notificationTypes.ENTITY_NOTIFICATION.value,
-    user: adminUserId,
-    createdBy: testUserId,
-    archived: false,
-    entityId: report3Id,
-    entityType: entityTypes.INSIGHT.value
-}];
+let entityNotifications = [
+    {
+        user: testUserId,
+        archived: false,
+        notificationType: notificationTypes.ENTITY_NOTIFICATION.value,
+        entityId: report2Id,
+        entityType: entityTypes.INSIGHT.value,
+        createdBy: adminUserId,
+    },
+    {
+        user: adminUserId,
+        archived: false,
+        notificationType: notificationTypes.ENTITY_NOTIFICATION.value,
+        entityId: report3Id,
+        entityType: entityTypes.INSIGHT.value,
+        createdBy: testUserId,
+    },
+];
 
 export { entityNotifications };
--- server/config/seeds/development/message-notifications.js
+++ server/config/seeds/development/message-notifications.js
@@ -1,19 +1,21 @@
 import { adminUserId, testUserId } from './users';
 import { notificationTypes } from '../../environment/shared';
 
-let messageNotifications = [{
-    notificationType: notificationTypes.MESSAGE_NOTIFICATION.value,
-    user: adminUserId,
-    createdBy: testUserId,
-    archived: false,
-    messageBody: 'test message body'
-},
-{
-    notificationType: notificationTypes.MESSAGE_NOTIFICATION.value,
-    user: testUserId,
-    createdBy: adminUserId,
-    archived: false,
-    messageBody: 'test message 2'
-}];
+let messageNotifications = [
+    {
+        user: adminUserId,
+        archived: false,
+        notificationType: notificationTypes.MESSAGE_NOTIFICATION.value,
+        message: 'test message body',
+        createdBy: testUserId,
+    },
+    {
+        user: testUserId,
+        archived: false,
+        notificationType: notificationTypes.MESSAGE_NOTIFICATION.value,
+        message: 'test message 2',
+        createdBy: adminUserId,
+    },
+];
 
 export { messageNotifications };
