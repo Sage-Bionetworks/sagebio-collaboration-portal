221fb67d8afbd30d8d401a2c83d4159b98f32cc9
tschaffter <thomas.schaffter@gmail.com>
2019-09-30 19:56:46 -0700

    Clean up app and project discussion page that list threads

--- client/app/discussion/discussion.component.ts
+++ client/app/discussion/discussion.component.ts
@@ -1,10 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
-import { AppService } from '../app.service';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 import { App } from 'models/entities/app.model';
-import { PageTitleService } from 'components/page-title/page-title.service';
+import { AppService } from '../app.service';
 import config from '../../app/app.constants';
-import { Thread } from 'models/messaging/thread.model';
 
 @Component({
     selector: 'discussion',
@@ -12,27 +11,15 @@ import { Thread } from 'models/messaging/thread.model';
     styles: [require('./discussion.scss')],
 })
 export class DiscussionComponent implements OnInit {
-    private app: App;
-    private appEntityType: string;
+    private app$: Observable<App>; // used in html
+    private entityType: string; // used in html
 
-    static parameters = [Router, ActivatedRoute, AppService, PageTitleService];
-    constructor(
-        private router: Router,
-        private route: ActivatedRoute,
-        private appService: AppService,
-        private pageTitleService: PageTitleService
-    ) {
-        this.appEntityType = config.entityTypes.APP.value;
+    static parameters = [Router, AppService];
+    constructor(private router: Router, private appService: AppService) {
+        this.entityType = config.entityTypes.APP.value;
     }
 
     ngOnInit() {
-        this.appService.getApp()
-            .subscribe(
-                app => {
-                    this.app = app;
-                    this.pageTitleService.title = `Discussion`;
-                },
-                err => console.error(err)
-            );
+        this.app$ = this.appService.getApp();
     }
 }
