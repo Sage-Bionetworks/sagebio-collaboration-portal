57b33fa2abb651a4f55c29b28c89032df449d070
James Eddy <james.a.eddy@gmail.com>
2019-10-14 22:36:55 -0700

    add more resource registration activity seeds

--- server/config/seeds/development/provenance.js
+++ server/config/seeds/development/provenance.js
@@ -138,6 +138,230 @@ let activities = [
                 subclass: 'Project'
             }
         ]
+    },
+    {
+        agents: [{
+            userId: adminUserId,
+            name: 'Admin',
+            role: ''
+        }],
+        description: '',
+        class: 'ResourceRegistration',
+        generated: [{
+            name: 'Systematic identification of mutations and copy number alterations associated with cancer patient prognosis',
+            role: '',
+            targetId: '5d41074d1233ee2c08977867',
+            targetVersionId: '1',
+            class: 'Resource',
+            subclass: 'Article'
+        }],
+        name: 'Creation of Article resource',
+        used: [
+            {
+                name: 'Test Project',
+                role: '',
+                targetId: '5cb7acea2d718614d81db97f',
+                targetVersionId: '1',
+                class: 'Project',
+                subclass: 'Project'
+            }
+        ]
+    },
+    {
+        agents: [{
+            userId: adminUserId,
+            name: 'Admin',
+            role: ''
+        }],
+        description: '',
+        class: 'ResourceRegistration',
+        generated: [{
+            name: 'Flatiron Dashboard',
+            role: '',
+            targetId: '5d3fca86fdf2999583f5f5f5',
+            targetVersionId: '1',
+            class: 'Resource',
+            subclass: 'Dashboard'
+        }],
+        name: 'Creation of Dashboard resource',
+        used: [
+            {
+                name: 'Admin Project',
+                role: '',
+                targetId: '5cb7acea2d718654d81bb97e',
+                targetVersionId: '1',
+                class: 'Project',
+                subclass: 'Project'
+            }
+        ]
+    },
+    {
+        agents: [{
+            userId: adminUserId,
+            name: 'Admin',
+            role: ''
+        }],
+        description: '',
+        class: 'ResourceRegistration',
+        generated: [{
+            name: 'Defining the boundaries of a Healthy Immune Response',
+            role: '',
+            targetId: '5d4108cf0457c62e907aa7bd',
+            targetVersionId: '1',
+            class: 'Resource',
+            subclass: 'WebApp'
+        }],
+        name: 'Creation of WebApp resource',
+        used: [
+            {
+                name: 'Admin Project',
+                role: '',
+                targetId: '5cb7acea2d718654d81bb97e',
+                targetVersionId: '1',
+                class: 'Project',
+                subclass: 'Project'
+            }
+        ]
+    },
+    {
+        agents: [{
+            userId: adminUserId,
+            name: 'Admin',
+            role: ''
+        }],
+        description: '',
+        class: 'ResourceRegistration',
+        generated: [{
+            name: 'Urrutia et al., 2016, Cell Reports - Interactive Application',
+            role: '',
+            targetId: '5d410e5aa70c0435f1bf2218',
+            targetVersionId: '1',
+            class: 'Resource',
+            subclass: 'WebApp'
+        }],
+        name: 'Creation of WebApp resource',
+        used: [
+            {
+                name: 'Admin Project',
+                role: '',
+                targetId: '5cb7acea2d718654d81bb97e',
+                targetVersionId: '1',
+                class: 'Project',
+                subclass: 'Project'
+            }
+        ]
+    },
+    {
+        agents: [{
+            userId: adminUserId,
+            name: 'Admin',
+            role: ''
+        }],
+        description: '',
+        class: 'ResourceRegistration',
+        generated: [{
+            name: 'Milieu Interieur sjTREC Prediction Service',
+            role: '',
+            targetId: '5d410e5aa70c0435f1bf221a',
+            targetVersionId: '1',
+            class: 'Resource',
+            subclass: 'WebApp'
+        }],
+        name: 'Creation of WebApp resource',
+        used: [
+            {
+                name: 'Admin Project',
+                role: '',
+                targetId: '5cb7acea2d718654d81bb97e',
+                targetVersionId: '1',
+                class: 'Project',
+                subclass: 'Project'
+            }
+        ]
+    },
+    {
+        agents: [{
+            userId: adminUserId,
+            name: 'Admin',
+            role: ''
+        }],
+        description: '',
+        class: 'ResourceRegistration',
+        generated: [{
+            name: 'The human immune response and the impact of age, sex, and genetics',
+            role: '',
+            targetId: '5d410e5aa70c0435f1bf221b',
+            targetVersionId: '1',
+            class: 'Resource',
+            subclass: 'WebApp'
+        }],
+        name: 'Creation of WebApp resource',
+        used: [
+            {
+                name: 'Admin Project',
+                role: '',
+                targetId: '5cb7acea2d718654d81bb97e',
+                targetVersionId: '1',
+                class: 'Project',
+                subclass: 'Project'
+            }
+        ]
+    },
+    {
+        agents: [{
+            userId: adminUserId,
+            name: 'Admin',
+            role: ''
+        }],
+        description: '',
+        class: 'ResourceRegistration',
+        generated: [{
+            name: 'CRI iAtlas Portal',
+            role: '',
+            targetId: '5d41112036398539a1e71f17',
+            targetVersionId: '1',
+            class: 'Resource',
+            subclass: 'WebApp'
+        }],
+        name: 'Creation of WebApp resource',
+        used: [
+            {
+                name: 'Test Project',
+                role: '',
+                targetId: '5cb7acea2d718614d81db97f',
+                targetVersionId: '1',
+                class: 'Project',
+                subclass: 'Project'
+            }
+        ]
+    },
+    {
+        agents: [{
+            userId: adminUserId,
+            name: 'Admin',
+            role: ''
+        }],
+        description: '',
+        class: 'ResourceRegistration',
+        generated: [{
+            name: 'Oncoscape',
+            role: '',
+            targetId: '5d41112036398539a1e71f18',
+            targetVersionId: '1',
+            class: 'Resource',
+            subclass: 'WebApp'
+        }],
+        name: 'Creation of WebApp resource',
+        used: [
+            {
+                name: 'Test Project',
+                role: '',
+                targetId: '5cb7acea2d718614d81db97f',
+                targetVersionId: '1',
+                class: 'Project',
+                subclass: 'Project'
+            }
+        ]
     }
 ];
 
