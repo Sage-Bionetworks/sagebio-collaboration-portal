3897c99dfbc6d9476841d912da632770d5d17013
tschaffter <thomas.schaffter@gmail.com>
2019-10-17 15:06:58 -0700

    Update dependencies (close #575)

--- server/config/environment/index.js
+++ server/config/environment/index.js
@@ -48,7 +48,7 @@ var all = {
             useNewUrlParser: true,
             useFindAndModify: false,
             useCreateIndex: true,
-            // useUnifiedTopology: true,
+            useUnifiedTopology: true,
             reconnectTries: 30,
             reconnectInterval: 500, // in ms
         }
